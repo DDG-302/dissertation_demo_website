a215b8accc7626c83ac2545ff992c9edda32326c
DDG-302 <2659384812@qq.com>
2024-02-24 18:19:43 +0800

    adjust page render

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ app = FastAPI()
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 TEMPLATE_LAYOUT_FILE = "website/layout.html"
+TEMPLATE_INDEX_FILE = "website/index.html"
+TEMPLATE_INDEX_BASIC_FILE = "website/text2sql.html"
 template_layout = templateEnv.get_template(TEMPLATE_LAYOUT_FILE)
+template_index = templateEnv.get_template(TEMPLATE_INDEX_FILE)
+template_index_basic = templateEnv.get_template(TEMPLATE_INDEX_BASIC_FILE)
+
+HTMLRESPONSE_index = template_layout.render({
+    "render_body": template_index.render({
+        "basic": template_index_basic.render()
+    })
+})
+
+async def set_index():
+    pass
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
-    with open("website/text2sql.html", "r", encoding="utf8") as f:
-        render_text = template_layout.render({
-            "render_body": f.read()
-        })
-    return render_text
+    return HTMLRESPONSE_index
 
 app.mount("/website", StaticFiles(directory="website"), name="web")
 if __name__ == "__main__":
