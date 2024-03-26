7aa33001d92a80d95a10315150f78459895f8216
DDG-302 <2659384812@qq.com>
2024-03-26 13:08:30 +0800

    move options from dialogue.py to options.py

---
+++ options.py
@@ -0,0 +1,11 @@
+class InputOptions:
+    NATURAL_LANGUAGE = "Natural Language"
+    SQL = "SQL"
+
+class DatabaseSchemaOptions:
+    JSONLIKE = "json like"
+    SQL = "CREATE statement"
+
+class ModelOptions:
+    RESDSQL = "RESDSQL"
+    GPT2 = "GPT-2"
