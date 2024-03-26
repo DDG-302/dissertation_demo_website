9af4c7326291a033282e6cf75c00a59371d6aaef
DDG-302 <2659384812@qq.com>
2024-03-26 13:08:03 +0800

    add more config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-database_dir = r"../bird/dev/dev_databases/dev_databases"
+database_dir = "../bird/dev/dev_databases/dev_databases"
 
-preprocessed_database_dir = r""
+table_json_file = "../bird/dev/dev_tables.json"
+resdsql_preprocessed_database_dir = "../bird/dev/dev_databases/resdsql_preprocessed_data/test_tables_for_natsql.json"
+
+dataframe_max_rows = 10
